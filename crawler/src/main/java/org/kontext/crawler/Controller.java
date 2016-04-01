f242c8d3ced955abca0b887985a3314a3f1cf495
Josh Petite <josh.petite@veritas.com>
2016-04-01 08:47:24 -0700

    Initial commit

---
+++ crawler/src/main/java/org/kontext/crawler/Controller.java
@@ -0,0 +1,43 @@
+package org.kontext.crawler;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class Controller {
+    private CrawlConfig config;
+    private final int numberOfCrawlers = 7;
+
+    public Controller() {
+        String crawlStorageFolder = "~/data/crawl/root";
+        config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+    }
+
+    public void start() throws Exception {
+        /*
+         * Instantiate the controller for this crawl.
+         */
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+        /*
+         * For each crawl, you need to add some seed urls. These are the first
+         * URLs that are fetched and then the crawler starts following links
+         * which are found in these pages
+         */
+        controller.addSeed("http://www.ics.uci.edu/~lopes/");
+        controller.addSeed("http://www.ics.uci.edu/~welling/");
+        controller.addSeed("http://www.ics.uci.edu/");
+
+        /*
+         * Start the crawl. This is a blocking operation, meaning that your code
+         * will reach the line after this only when crawling is finished.
+         */
+        controller.start(InitialCrawler.class, numberOfCrawlers);
+    }
+}
