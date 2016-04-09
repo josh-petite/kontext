94593bc758009cd69c5c7fdf2119721fb7a92f6b
viki85in <vikam.s@gmail.com>
2016-04-09 00:41:24 -0700

    Skeleton for parser.

--- common/src/main/java/org/kontext/common/repositories/PropertiesRepository.java
+++ common/src/main/java/org/kontext/common/repositories/PropertiesRepository.java
@@ -3,7 +3,26 @@ package org.kontext.common.repositories;
 import java.io.IOException;
 
 public interface PropertiesRepository {
-    String read(String key);
-    void write(String key, String value);
-    void save() throws IOException;
+	
+	/**
+	 * Read the value for the property key passed.
+	 * 
+	 * @param key
+	 * @return
+	 */
+	String read(String key);
+
+	/**
+	 * Write the value of the property if it is changeable.
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	void write(String key, String value);
+
+	/**
+	 * Save the entire property set and make it retrievable.
+	 * @throws IOException
+	 */
+	void save() throws IOException;
 }
---
+++ parser/src/main/java/org/kontext/parser/ContentParser.java
@@ -0,0 +1,25 @@
+package org.kontext.parser;
+
+import org.kontext.common.repositories.PropertiesRepository;
+
+import com.google.inject.Inject;
+
+/**
+ * Content parser takes in a batch of documents and parses the contents. Parser
+ * shall be called when the subscriber (controller) of the batches find that a
+ * batch is ready for parsing. Parsed output pertaining to a document shall be 
+ * stored against the document itself for further processing.
+ */
+public class ContentParser implements Runnable {
+	
+	private PropertiesRepository propsRepo;
+	@Inject
+	public ContentParser(PropertiesRepository propsRepo) {
+		this.propsRepo = propsRepo;
+	}
+
+	@Override
+	public void run() {
+	}
+
+}
